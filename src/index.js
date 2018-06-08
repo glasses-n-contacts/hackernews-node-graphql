29ca0654ffdcadfa2e51d875714655e9477538ad
Bill Yu <han.yu@duke.edu>
2018-06-08 22:25:18 +0800

    prisma+graphql

---
+++ src/index.js
@@ -0,0 +1,43 @@
+const { GraphQLServer } = require('graphql-yoga');
+
+// 2
+// 1
+let links = [{
+  id: 'link-0',
+  url: 'www.howtographql.com',
+  description: 'Fullstack tutorial for GraphQL',
+}];
+let idCount = links.length;
+
+const resolvers = {
+  Query: {
+    info: () => `This is the API of a Hackernews Clone`,
+    // 2
+    feed: () => links,
+  },
+  Mutation: {
+    // 2
+    post: (root, args) => {
+      const link = {
+        id: `link-${idCount++}`,
+        description: args.description,
+        url: args.url,
+      };
+      links.push(link);
+      return link;
+    },
+  },
+  // 3
+  Link: {
+    id: (root) => root.id,
+    description: (root) => root.description,
+    url: (root) => root.url,
+  },
+};
+
+// 3
+const server = new GraphQLServer({
+  typeDefs: './src/schema.graphql',
+  resolvers,
+});
+server.start(() => console.log(`Server is running on http://localhost:4000`));
